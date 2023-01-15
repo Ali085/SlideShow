a06d90a161c5b1eb207d5da86b2371e79491a9d5
unknown <ali085>
2023-01-15 12:38:42 -0800

    SlideShowGalery

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const immag = [
+    { src: "https://nationaltoday.com/wp-content/uploads/2021/12/Annoy-Squidward-Day-300x300.jpg" },
+    { src: "https://www.dictionary.com/e/wp-content/uploads/2021/08/20210830_prayerHands_800x800-300x300.png" },
+    { src: "http://www.deepdiveintel.com/wp-content/uploads/2013/09/Owl-Eye-art-300x300.jpg" }
+  ]
+  const [ImageVal, setImageVal] = useState(immag[0])
+  const Change = (sum) => {
+    const slider = immag[sum]
+    setImageVal(slider)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='Header'>
+        <img src={ImageVal.src} />
+      </div>
+      <div className='bottomDiv'>
+        {
+          immag.map((data, e) =>
+            <div className='fotos-click'>
+
+              <img src={data.src} onClick={() => Change(e)} />
+
+            </div>
+          )
+        }
+      </div>
+    </>
   );
 }
 
